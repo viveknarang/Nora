e85ffd43c506df137678463ff230a4b69d76403d
Vivek Narang <vivek.narang10@gmail.com>
2018-05-26 23:11:08 -0700

    CRITICAL: rule sequencing, ordering, rule template modification ...

--- src/main/java/com/viveknarang/nora/main/Transformer.java
+++ src/main/java/com/viveknarang/nora/main/Transformer.java
@@ -2,9 +2,8 @@ package com.viveknarang.nora.main;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.TreeMap;
 import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
@@ -14,7 +13,7 @@ import com.viveknarang.nora.model.Rules;
 
 public class Transformer {
 
-	private static Map<Integer, Rules> rulesMap = new LinkedHashMap<Integer, Rules>();
+	private static HashMap<Integer, TreeMap<Integer, List<String>>> rulesMap = new HashMap<Integer, TreeMap<Integer, List<String>>>();
 
 	final static Logger logger = Logger.getLogger(Transformer.class);
 
@@ -25,13 +24,21 @@ public class Transformer {
 	public static void transform(Job job, List<Rules> rules, List<String[]> rows, String fileName) {
 		logger.info("Transformer:transform()::Start");
 		long s = System.currentTimeMillis();
-		rulesMap = new HashMap<Integer, Rules>();
+		rulesMap = new HashMap<Integer, TreeMap<Integer, List<String>>>();
 		int i = 0;
 
 		for (Rules rule : rules) {
+
+			TreeMap<Integer, List<String>> rules$ = new TreeMap<Integer, List<String>>();
 			if (rule.getFileName().equals(fileName)) {
-				rulesMap.put(rule.getMapIndex(), rule);
+
+				for (int ti = 0; ti < rule.getTransform().size(); ti++) {
+					rules$.put(Integer.parseInt(rule.getTransform().get(ti).get(0)), rule.getTransform().get(ti));
+				}
+
+				rulesMap.put(rule.getMapIndex(), rules$);
 			}
+
 		}
 
 		for (String[] row : rows) {
@@ -44,68 +51,50 @@ public class Transformer {
 		}
 
 		System.out.println(Arrays.toString(rows.get(0)));
-		System.out.println(Arrays.toString(rows.get(1)));
-		System.out.println(Arrays.toString(rows.get(2)));
-		System.out.println(Arrays.toString(rows.get(3)));
-		System.out.println(Arrays.toString(rows.get(4)));
-		System.out.println(Arrays.toString(rows.get(5)));
-		System.out.println(Arrays.toString(rows.get(6)));
-		System.out.println(Arrays.toString(rows.get(7)));
-		System.out.println(Arrays.toString(rows.get(8)));
-		System.out.println(Arrays.toString(rows.get(9)));
-		
-		long e = System.currentTimeMillis();
 
+		long e = System.currentTimeMillis();
 		logger.info("Transformer:transform()::Complete >> Transformation completed in: " + ((e - s) / 1000)
 				+ " seconds for a total of: " + rows.size() + " records");
 	}
 
-	public static String transform(String field, Rules rule) {
+	public static String transform(String field, TreeMap<Integer, List<String>> rule_) {
 
-		if (field.length() == 0 && rule.getReplaceNullWith()[1].equals("true")) {
-			field = rule.getReplaceNullWith()[2];
-		} else {
+		for (Integer rule : rule_.keySet()) {
+			if (rule_.get(rule).get(1).equals("replaceNullWith") && field.length() == 0
+					&& rule_.get(rule).get(2).equals("true")) {
+				field = rule_.get(rule).get(3);
+			} else {
 
-			if (rule.getTruncateAfter()[1].equals("true")) {
-				if (field.length() > Integer.parseInt(rule.getTruncateAfter()[2])) {
-					field = field.substring(0, Integer.parseInt(rule.getTruncateAfter()[2])) + rule.getTruncateAfter()[3];
+				if (rule_.get(rule).get(1).equals("truncateAfter") && rule_.get(rule).get(2).equals("true")) {
+					if (field.length() > Integer.parseInt(rule_.get(rule).get(3))) {
+						field = field.substring(0, Integer.parseInt(rule_.get(rule).get(3))) + rule_.get(rule).get(4);
+					}
+				} else if (rule_.get(rule).get(1).equals("replace") && rule_.get(rule).get(2).equals("true")) {
+					field = field.replaceAll(rule_.get(rule).get(3), rule_.get(rule).get(4));
+				} else if (rule_.get(rule).get(1).equals("remove") && rule_.get(rule).get(2).equals("true")) {
+					field = field.replace(rule_.get(rule).get(3), "");
+				} else if (rule_.get(rule).get(1).equals("extractFromEndTo") && rule_.get(rule).get(2).equals("true")
+						&& field.contains(rule_.get(rule).get(3))) {
+					String[] px = field.split(Pattern.quote(rule_.get(rule).get(3)));
+					field = px[px.length - 1];
+				} else if (rule_.get(rule).get(1).equals("extractFromFrontTo") && rule_.get(rule).get(2).equals("true")
+						&& field.contains(rule_.get(rule).get(3))) {
+					String[] px = field.split(Pattern.quote(rule_.get(rule).get(3)));
+					field = px[0];
+				} else if (rule_.get(rule).get(1).equals("trim") && rule_.get(rule).get(2).equals("true")) {
+					field = field.trim();
+				} else if (rule_.get(rule).get(1).equals("toUpperCase") && rule_.get(rule).get(2).equals("true")) {
+					field = field.toUpperCase();
+				} else if (rule_.get(rule).get(1).equals("toLowerCase") && rule_.get(rule).get(2).equals("true")) {
+					field = field.toLowerCase();
+				} else if (rule_.get(rule).get(1).equals("extractFromTo") && rule_.get(rule).get(2).equals("true")
+						&& field.length() > Integer.parseInt(rule_.get(rule).get(4))) {
+					field = field.substring(Integer.parseInt(rule_.get(rule).get(3)),
+							Integer.parseInt(rule_.get(rule).get(4)));
 				}
-			}
-			
-			if (rule.getReplace()[1].equals("true")) {
-				field = field.replaceAll(rule.getReplace()[2], rule.getReplace()[3]);
-			}
-			
-			if (rule.getRemove()[1].equals("true")) {
-				field = field.replace(rule.getRemove()[2], "");
-			}
-			
-			if (rule.getExtractFromEndTo()[1].equals("true") && field.contains(rule.getExtractFromEndTo()[2])) {
-				String[] px = field.split(Pattern.quote(rule.getExtractFromEndTo()[2]));
-				field = px[px.length-1];
+
 			}
 
-			if (rule.getExtractFromFrontTo()[1].equals("true") && field.contains(rule.getExtractFromFrontTo()[2])) {
-				String[] px = field.split(Pattern.quote(rule.getExtractFromFrontTo()[2]));
-				field = px[0];
-			}
-
-			if (rule.getTrim()[1].equals("true")) {
-				field = field.trim();
-			}
-			
-			if (rule.getToUpperCase()[1].equals("true")) {
-				field = field.toUpperCase();
-			}
-
-			if (rule.getToLowerCase()[1].equals("true")) {
-				field = field.toLowerCase();
-			}
-
-			if (rule.getExtractFromTo()[1].equals("true") && field.length() > Integer.parseInt(rule.getExtractFromTo()[3])) {
-				field = field.substring(Integer.parseInt(rule.getExtractFromTo()[2]), Integer.parseInt(rule.getExtractFromTo()[3]));
-			}
-			
 		}
 
 		return field;
--- src/main/java/com/viveknarang/nora/model/Rules.java
+++ src/main/java/com/viveknarang/nora/model/Rules.java
@@ -1,5 +1,7 @@
 package com.viveknarang.nora.model;
 
+import java.util.List;
+
 public class Rules {
 
 	private String fileName = null;
@@ -7,18 +9,7 @@ public class Rules {
 	private String collectionName = null;
 	private String mapToField = null;
 	private int mapIndex = -1;
-
-	private String[] truncateAfter = null;
-	private String[] replaceNullWith = null;
-	private String[] replace = null;
-	private String[] remove = null;
-	private String[] extractFromEndTo = null;
-	private String[] extractFromFrontTo = null;
-	private String[] trim = null;
-	private String[] removeAllSpaces = null;
-	private String[] toLowerCase = null;
-	private String[] toUpperCase = null;
-	private String[] extractFromTo = null;
+	private List<List<String>> transform = null;
 
 	public String getFileName() {
 		return fileName;
@@ -40,48 +31,8 @@ public class Rules {
 		return mapIndex;
 	}
 
-	public String[] getTruncateAfter() {
-		return truncateAfter;
-	}
-
-	public String[] getReplaceNullWith() {
-		return replaceNullWith;
-	}
-
-	public String[] getReplace() {
-		return replace;
-	}
-
-	public String[] getRemove() {
-		return remove;
-	}
-
-	public String[] getExtractFromEndTo() {
-		return extractFromEndTo;
-	}
-
-	public String[] getExtractFromFrontTo() {
-		return extractFromFrontTo;
-	}
-
-	public String[] getTrim() {
-		return trim;
-	}
-
-	public String[] getRemoveAllSpaces() {
-		return removeAllSpaces;
-	}
-
-	public String[] getToLowerCase() {
-		return toLowerCase;
-	}
-
-	public String[] getToUpperCase() {
-		return toUpperCase;
-	}
-
-	public String[] getExtractFromTo() {
-		return extractFromTo;
+	public List<List<String>> getTransform() {
+		return transform;
 	}
 
 }

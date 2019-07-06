527ceb112a038706d1ff354b152a2c17126ce5f4
Vivek Narang <vivek.narang10@gmail.com>
2019-07-06 15:49:34 -0400

    New Feature: Ability to split a field and dump as a list in Mongo ...

--- src/main/java/com/viveknarang/nora/main/Transformer.java
+++ src/main/java/com/viveknarang/nora/main/Transformer.java
@@ -5,10 +5,7 @@ import com.viveknarang.nora.model.Rule;
 import org.apache.log4j.Logger;
 import org.bson.Document;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.regex.Pattern;
 
 /**
@@ -114,46 +111,78 @@ public class Transformer {
 
         }
 
+        System.out.println(transformedRows.get(0).toJson());
+
         long e = System.currentTimeMillis();
         logger.info("Transformer:transform()::Complete >> Transformation completed in: " + ((e - s) / 1000)
                 + " seconds on a total of: " + rows.size() + " records & " + rulesMap.size() + " fields...");
 
     }
 
-    private static String transform(String field, TreeMap<Integer, List<String>> rule_) {
+    private static Object transform(String field, TreeMap<Integer, List<String>> rule_) {
+
 
         for (Integer rule : rule_.keySet()) {
+
             if (rule_.get(rule).get(1).equals("replaceNullWith") && field.length() == 0
                     && rule_.get(rule).get(2).equals("true")) {
+
                 field = rule_.get(rule).get(3);
+
             } else {
 
-                if (rule_.get(rule).get(1).equals("truncateAfter") && rule_.get(rule).get(2).equals("true")) {
+                if (rule_.get(rule).get(1).equals("prepareList")) {
+
+                    List<String> transformed = new LinkedList<>(Arrays.asList(field.trim().split(rule_.get(rule).get(2))));
+                    return transformed;
+
+                } else if (rule_.get(rule).get(1).equals("truncateAfter") && rule_.get(rule).get(2).equals("true")) {
+
+
                     if (field.length() > Integer.parseInt(rule_.get(rule).get(3))) {
                         field = field.substring(0, Integer.parseInt(rule_.get(rule).get(3))) + rule_.get(rule).get(4);
                     }
+
                 } else if (rule_.get(rule).get(1).equals("replace") && rule_.get(rule).get(2).equals("true")) {
+
+
                     field = field.replaceAll(rule_.get(rule).get(3), rule_.get(rule).get(4));
+
                 } else if (rule_.get(rule).get(1).equals("remove") && rule_.get(rule).get(2).equals("true")) {
+
+
                     field = field.replace(rule_.get(rule).get(3), "");
+
                 } else if (rule_.get(rule).get(1).equals("extractFromEndTo") && rule_.get(rule).get(2).equals("true")
                         && field.contains(rule_.get(rule).get(3))) {
                     String[] px = field.split(Pattern.quote(rule_.get(rule).get(3)));
+
                     field = px[px.length - 1];
+
                 } else if (rule_.get(rule).get(1).equals("extractFromFrontTo") && rule_.get(rule).get(2).equals("true")
                         && field.contains(rule_.get(rule).get(3))) {
                     String[] px = field.split(Pattern.quote(rule_.get(rule).get(3)));
+
                     field = px[0];
+
                 } else if (rule_.get(rule).get(1).equals("trim") && rule_.get(rule).get(2).equals("true")) {
+
                     field = field.trim();
+
                 } else if (rule_.get(rule).get(1).equals("toUpperCase") && rule_.get(rule).get(2).equals("true")) {
+
                     field = field.toUpperCase();
+
                 } else if (rule_.get(rule).get(1).equals("toLowerCase") && rule_.get(rule).get(2).equals("true")) {
+
                     field = field.toLowerCase();
+
                 } else if (rule_.get(rule).get(1).equals("extractFromTo") && rule_.get(rule).get(2).equals("true")
                         && field.length() > Integer.parseInt(rule_.get(rule).get(4))) {
+
                     field = field.substring(Integer.parseInt(rule_.get(rule).get(3)),
                             Integer.parseInt(rule_.get(rule).get(4)));
+
                 }
 
             }
